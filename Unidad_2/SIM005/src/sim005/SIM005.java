2695606902fcd61040681f5d3d48841e375a2ca6
Roberto Yahir Hernandez Almazan <roberto00hernamdez@gmail.com>
2026-05-03 23:08:54 -0600

    agregando ejercicio 5

---
+++ Unidad_2/SIM005/src/sim005/SIM005.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sim005;
+
+/**
+ *
+ * @author yahir
+ */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class SIM005 extends JFrame {
+
+    public SIM005() {
+        setTitle("Ejercicio 5: MouseListener");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridBagLayout());
+        setLocationRelativeTo(null);
+
+        JLabel etiqueta = new JLabel("Pasa el ratón por aquí", SwingConstants.CENTER);
+        etiqueta.setPreferredSize(new Dimension(200, 50));
+        etiqueta.setOpaque(true);
+        etiqueta.setBackground(Color.BLUE);
+
+        etiqueta.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                etiqueta.setBackground(Color.RED);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                etiqueta.setBackground(Color.BLUE);
+            }
+        });
+
+        add(etiqueta);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new SIM005().setVisible(true);
+        });
+    }
+}
